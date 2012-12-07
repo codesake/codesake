a427e5efd77e6e5d3daff8e7e1261b31a29148c6
Paolo Perego <thesp0nge@gmail.com>
2012-12-07 09:21:53 +0100

    Now there is a Codesake::Utils::Secrets

--- lib/codesake/utils/secrets.rb
+++ lib/codesake/utils/secrets.rb
@@ -1,7 +1,35 @@
 module Codesake
   module Utils
     module Secrets
-      attr_accessor :sensitive_words = ["secret", "password", "username", "login", "xxx", "fixme", "fix", "todo", "passwd"]
+      DEFAULT_SECRETS = ["secret", "password", "username", "login", "xxx", "fixme", "fix", "todo", "passwd"]
+      attr_accessor :secrets
+
+      def initialize
+        @secrets = DEFAULT_SECRETS
+      end
+
+      def add(word)
+        @secrets << word
+        @secrets
+      end
+
+      def reserved?(word)
+        @secrets.include?(word)
+      end
+
+      def scan
+
+        ret = []
+
+        self.file_content.each_with_index do |l, i|
+          l = l.unpack("C*").pack("U*")
+          l.split.each do |tok|
+            ret << {:line=> i+1, :matcher=>tok, :source_line=>l} if @secrets.include?(tok.downcase)
+          end
+        end
+
+        ret
+      end
 
 
 
--- spec/secrets_utils_spec.rb
+++ spec/secrets_utils_spec.rb
@@ -6,9 +6,14 @@ This is a text
 This is a text 
 This is a text 
 This is a text 
+secret
+This is a text 
+This is a text 
+This is a text 
 EOT
 
 class Test
+  include Codesake::Utils::Files
   include Codesake::Utils::Secrets
 end
 
@@ -16,27 +21,59 @@ end
 shared_examples_for Codesake::Utils::Secrets do
   before(:all) do
     @mock = Test.new
+    File.open("test_secrets.txt", "w") do |f|
+        f.puts(dummy_text)
+      end
+
+    @mock.filename="test_secrets.txt"
+    @mock.read_file
+  end
+  after(:all) do
+    File.delete("test_secrets.txt")
   end
 
   context "has a list of reserved words containing" do
     it "password" do
-      @mock.sensitive_words.include?("password").should be_true
+      @mock.reserved?("password").should be_true
     end
-    it "username"
-    it "login"
-    it "fixme"
-    it "xxx"
-    it "fix"
+    it "username" do
+      @mock.reserved?("username").should be_true
+    end
+    it "login" do
+      @mock.reserved?("login").should be_true
+    end
+    it "fixme" do
+      @mock.reserved?("fixme").should be_true
+    end
+    it "xxx" do
+      @mock.reserved?("xxx").should be_true
+    end
+    it "fix" do
+      @mock.reserved?("fix").should be_true
+    end
   end
 
-  context "has a find_secrets method" do
-    it "and this method is public"
-    it "returns true if 'Donec' is a reserved word in the example source"
-    it "returns true if 'donec' is a reserved word in the example source" 
-    it "returns true if 'eRaT' is a reserved word in the example source" 
-    it "returns false if 'password' is a reserved word in the example source"
-    it "returns false if 'ccv' is a reserved word in the example source"
-    it "returns false if 'login' is a reserved word in the example source"
+  it "let user add a reserver word" do
+    @mock.reserved?("foo").should be_false
+    @mock.add("foo")
+    @mock.reserved?("foo").should be_true
+
+  end
+  context "has a find method" do
+    it "that is public" do
+      @mock.should  respond_to(:scan)
+    end
+    it "returns an empty array if 'secret' is not a reserved word in the example source" do
+      @mock.secrets=["noodle", "compain", "foo"]
+      @mock.scan.should be_empty
+    end
+    it "returns true if 'secret' is a reserved word in the example source" do
+      @mock.secrets=["secret"]
+      @mock.scan.should == [{:line=>5, :matcher=>"secret", :source_line=>"secret\n"}]
+    end
+
+
   end
 
+  
 end
