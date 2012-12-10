2829fa06691e5368fa89fb356828dba617b89981
Paolo Perego <thesp0nge@gmail.com>
2012-12-10 08:55:03 +0100

    CLI parser improvements

--- lib/codesake/cli.rb
+++ lib/codesake/cli.rb
@@ -1,7 +1,42 @@
 require 'optparse'
+require 'trimmy'
 
 module Codesake
   class Cli
+    
+    def parse(command_line)
+      ret = {}
+
+      return {} if (command_line.nil?) or (command_line.trim.empty?)
+
+      begin
+        opts=OptionParser.new
+        opts.on("-h", "--help") do 
+          ret[:help] = true 
+        end
+
+        opts.on("-v", "--version") do
+          ret[:version] = true
+        end
+
+        opts.on("-V", "--verbose") do
+          ret[:verbose] = true
+        end
+
+        opts.on("-o VAL", "--output", "Write output to file, to json string or to db usin SQLite3") do |val|
+          ret[:output]=:screen
+          val=val.trim
+          ret[:output]=val.to_sym if (val.to_sym == :file) or (val.to_sym == :json) or (val.to_sym == :db)
+        end
+
+
+        rest = opts.parse(command_line)
+        ret[:target] = rest if (! ret[:help] ) and ( ! ret[:version]) and (! rest.empty?)
+      rescue OptionParser::InvalidOption => e
+        ret={:error=>true, :message=>e.message}
+      end
+      ret
+    end
 
   end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,19 +4,46 @@ describe "codesake command line interface" do
   before(:all) do
     @cli = Codesake::Cli.new
   end
-  it "has a parse method" 
-  # do
-  #   @cli.should respond_to(:scan)
-  # end
+  it "has a parse method" do
+    @cli.should respond_to(:parse)
+  end
 
-  it "understands --help flag" 
-  it "understands --version flag"
-  it "understands --output flag"
-  it "understands --verbose flag"
+  it "returns an empty has if no command line is given" do
+    @cli.parse(nil).should    be_empty
+    @cli.parse("").should     be_empty
+    @cli.parse(" ").should    be_empty
+    @cli.parse("  ").should   be_empty
+  end
 
-  it "must have at least an argument"
-  it "the argument should be either a file or a directory"
-  it "must produce a non nil option Hash"
+  it "understands --help flag" do
+    @cli.parse("-h").should ==  {:help=>true}
+    @cli.parse("--help").should ==  {:help=>true}
+  end 
+  it "undestands malformed flag" do 
+    @cli.parse("-help").should == {:error=>true, :message=>"invalid option: -elp"}
+  end
+
+  it "understands --version flag" do
+    @cli.parse("--version").should == {:version=>true}
+  end
+  it "understands --output flag" do
+    @cli.parse("-o file").should  ==  {:output=>:file}
+    @cli.parse("-o json").should  ==  {:output=>:json}
+    @cli.parse("-o db").should    ==  {:output=>:db}
+
+  end
+  it "understands --verbose flag" do
+    @cli.parse("--verbose").should == {:verbose=>true}
+    @cli.parse("-V").should == {:verbose=>true}
+  end
+
+  it "the target should be either a file or a directory"
+  it "must produce a non nil option Hash" do
+    @cli.parse("").should_not           be_nil
+    @cli.parse("-nonsense").should_not  be_nil
+    @cli.parse("-v -h").should_not      be_nil
+    @cli.parse("-j drift").should_not   be_nil
+  end
 
 
 end
